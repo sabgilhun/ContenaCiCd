8cf2089ccc0d6d71f975c0026ab1e63b5555859c
sabgilhun <sabgilhun@gmail.com>
2020-08-21 23:23:45 +0900

    add htpasswd gitignore

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
+if [ "$#" -lt 1 ]; then
+    echo 'Require parameter "id", "password"'
+	exit 1
+fi
+
 mkdir jenkins/jenkins_home
 chown 1000:1000 jenkins/jenkins_home
+
+docker run --rm --entrypoint htpasswd registry:2.6.2 -Bbn $1 $2 > auth/nginx.htpasswd
