b09dbce12dc308888fc997b121f8dd10b6b46114
sabgilhun <sabgilhun@gmail.com>
2020-08-21 23:35:02 +0900

    modify shell script

--- init.sh
+++ init.sh
@@ -6,4 +6,10 @@ fi
 mkdir jenkins/jenkins_home
 chown 1000:1000 jenkins/jenkins_home
 
+mkdir auth
+
+mkdir nginx
+
+mkdir registry
+
 docker run --rm --entrypoint htpasswd registry:2.6.2 -Bbn $1 $2 > auth/nginx.htpasswd
